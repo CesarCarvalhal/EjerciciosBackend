d076ffc678f1f94f8821e7171815352dd045676c
CesarCarvalhalT <113435339+CesarCarvalhalT@users.noreply.github.com>
2022-10-13 12:56:53 +0200

    Add files via upload

---
+++ sprint2python/concurso.py
@@ -0,0 +1,16 @@
+#Mostrar pregunta con 3 respuestas posibles
+
+print("¿Cuál es el nombre de la mascota de la familia Simpson?")
+print("a) Santa's Little Helper")
+print("b) Snowball II")
+print("c) Snowball I")
+print("d) Santa's Little Helper II")
+
+#Leer respuesta del usuario
+respuesta = input("Respuesta: ")
+
+#Mostrar si la respuesta es correcta o no
+if respuesta == "a":
+    print("Correcto")
+else:
+    print("Incorrecto")
