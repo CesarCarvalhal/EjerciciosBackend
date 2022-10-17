32ed6ad93392ee40ecb1b54ad50096ccf9a71861
César Carvalhal <ccarvalhalt22@fpcoruna.afundacion.org>
2022-10-17 12:27:47 +0200

    EJ 4

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,4 +1,7 @@
 from factorial import func_factorial
+from factorial2 import func_factorial2
 
 numero = int(input("Introduce un número: "))
 print("El factorial de", numero, "es", func_factorial(numero))
+print()
+print("El factorial de", numero, "es", func_factorial2(numero))
