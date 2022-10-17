d22e9a985592e3cd7145a786faa604e0379ebd42
César Carvalhal <ccarvalhalt22@fpcoruna.afundacion.org>
2022-10-17 12:49:49 +0200

    EJ5

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,7 +1,14 @@
 from factorial import func_factorial
 from factorial2 import func_factorial2
 
-numero = int(input("Introduce un número: "))
-print("El factorial de", numero, "es", func_factorial(numero))
-print()
-print("El factorial de", numero, "es", func_factorial2(numero))
+print("a) Funcion factorial con recursividad")
+print("b) Funcion factorial sin recursividad")
+opcion = input("Elige una opcion: ")
+if opcion == "a":
+    numero = int(input("Introduce un número: "))
+    print("El factorial de", numero, "es", func_factorial(numero))
+elif opcion == "b":
+    numero = int(input("Introduce un número: "))
+    print("El factorial sin recursividad de", numero, "es", func_factorial2(numero))
+else:
+    print("Opcion incorrecta")
