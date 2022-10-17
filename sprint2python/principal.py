228813482398c01e13d4b655696b526bcec2063c
César Carvalhal <ccarvalhalt22@fpcoruna.afundacion.org>
2022-10-17 13:03:45 +0200

    EJ6

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,14 +1,25 @@
 from factorial import func_factorial
 from factorial2 import func_factorial2
 
+import time
+
 print("a) Funcion factorial con recursividad")
 print("b) Funcion factorial sin recursividad")
 opcion = input("Elige una opcion: ")
-if opcion == "a":
+
+if opcion == 'a':
     numero = int(input("Introduce un número: "))
+    start_time = time.time()
     print("El factorial de", numero, "es", func_factorial(numero))
-elif opcion == "b":
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
+elif opcion == 'b':
     numero = int(input("Introduce un número: "))
+    start_time = time.time()
     print("El factorial sin recursividad de", numero, "es", func_factorial2(numero))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 else:
-    print("Opcion incorrecta")
+    print('Opción incorrecta')
