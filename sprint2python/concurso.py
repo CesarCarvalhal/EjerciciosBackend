fb524885816bdaae86719e4e50709cf52c6414ca
César Carvalhal <ccarvalhalt22@fpcoruna.afundacion.org>
2022-10-18 10:13:08 +0200

    EJ7

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,55 +1,65 @@
-﻿#Se muestra una pregunta, con 3 respuestas posibles
-print("¿Cuál es el nombre del perro de la familia Simpson?")
-print("a) Santa's Little Helper")
-print("b) Snowball II")
-print("c) Snowball I")
+﻿import random
 
-#Se lee la respuesta del usuario
-respuesta = input("Respuesta: ")
+def preguntas(n):
+    contador = 0
+    
+    if n == 1:
+        print("¿Quién ganó la primera edición de la Champions League?")
+        print("a) Real Madrid") #Correcta
+        print("b) Bayern Munich")
+        print("c) Juventus")
+        
+        respuesta = input("Respuesta: ")
+        
+        if respuesta == "a":
+            print("¡Correcto!")
+            contador = contador + 10
+        else:
+            print("¡Incorrecto!")
+            contador = contador - 5
+            print()
 
-#Se comprueba y se muestra si la respuesta es correcta o no
-contador = 0
-if respuesta == "a":
-    print("Correcto")
-    contador = contador + 10
-else:
-    print("Incorrecto")
-    contador = contador - 5
+    if n == 2:
 
-print()
+            print("La serie de Los Simpsons ha emitido un total de…")
+            print("a) 583") #Correcta
+            print("b) 696")
+            print("c) 417")
+            
+            respuesta = input("Respuesta: ")
+
+            if respuesta == "a":
+                print("¡Correcto!")
+                contador = contador + 10
+            else:
+                print("¡Incorrecto!")
+                contador = contador - 5
+                print()
+
+    if n == 3:
+            print("Los únicos personajes que han sido dibujados con cinco dedos en la serie son…")
+            print("a) Dios y Jesucristo") #Correcta
+            print("b) Paul McCartney y Yoko Ono")
+            print("c) Bruce Springsteen y Lady Gaga")
+
+            respuesta = input("Respuesta: ")
+
+            if respuesta == "a":
+                print("¡Correcto!")
+                contador = contador + 10
+            else:
+                print("¡Incorrecto!")
+                contador = contador - 5
+                print()
+    return contador
 
 
-print("Elija la opción correcta: La serie ha emitido un total de…")
-print("a) 583") #Correcta
-print("b) 696")
-print("c) 417")
-respuesta = input("Respuesta: ")
+n1 = random.randint(1,3)
+contador1 = preguntas(n1)
+n2 = random.randint(1,3)
+while n1 == n2:
+    n2 = random.randint(1,3)
+contador2 = preguntas(n2)
 
-if respuesta == "a":
-    print("Correcto")
-    contador = contador + 10
-else:
-    print("Incorrecto")
-    contador = contador - 5
 
-print()
-
-print("Los únicos personajes que han sido dibujados con cinco dedos en la serie son…")
-print("a) Dios y Jesucristo") #Correcta
-print("b) Paul McCartney y Yoko Ono")
-print("c) Bruce Springsteen y Lady Gaga")
-
-#Se lee la respuesta del usuario
-respuesta = input("Respuesta: ")
-
-#Por cada pregunta correcta se suma 10 al contador, y por cada incorrecta se resta 5
-if respuesta == "a":
-    print("Correcto")
-    contador = contador + 10
-else:
-    print("Incorrecto")
-    contador = contador - 5
-print()
-
-#calcular puntuacion
-print("La puntuación es: ", contador)
+print("La puntuación final es de: ", contador1 +contador2)
