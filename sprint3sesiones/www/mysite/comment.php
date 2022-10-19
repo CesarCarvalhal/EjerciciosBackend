ac8bbf526a34c5423c713f06767dd779c371f0d8
César Carvalhal <ccarvalhalt22@fpcoruna.afundacion.org>
2022-10-19 14:43:00 +0200

    EJ4

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -2,21 +2,23 @@
 $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <html>
-    <body>
-        <?php
-        $juego_id = $_POST['juego_id'];
-        $comentario = $_POST['new_comment'];
-        
-        $query = "INSERT INTO tComentarios(comentario, juego_id, usuario_id, fecha_comentario)
-VALUES ('".$comentario."',".$juego_id.",NULL, now())";
-    mysqli_query($db, $query) or die('Error');
-
-    echo "<p>Nuevo comentario ";
-    echo mysqli_insert_id($db);
-    echo " añadido</p>";
-    
-    echo "<a href='/detail.php?juego_id=".$juego_id."'>Volver</a>";
-    mysqli_close($db);
-        ?>
-    </body>
+<body>
+<?php
+session_start();
+$user_id_a_insertar = 'NULL';
+if (!empty($_SESSION['user_id'])) {
+$user_id_a_insertar = $_SESSION['user_id'];
+}
+$juego_id = $_POST['juego_id'];
+$comentario = $_POST['new_comment'];
+$query = "INSERT INTO tComentarios(comentario, juego_id, usuario_id)
+VALUES ('".$comentario."',".$juego_id.",".$user_id_a_insertar.")";
+mysqli_query($db, $query) or die('Error');
+echo "<p>Nuevo comentario ";
+echo mysqli_insert_id($db);
+echo " añadido</p>";
+echo "<a href='/detail.php?juego_id=".$juego_id."'>Volver</a>";
+mysqli_close($db);
+?>
+</body>
 </html>
