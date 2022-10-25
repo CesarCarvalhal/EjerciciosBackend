5c1377418d5f778cf99a14c76e60c31d3303f477
César Carvalhal <ccarvalhalt22@fpcoruna.afundacion.org>
2022-10-25 13:52:31 +0200

    EJ 7 y REORGANIZACIÓN GENERAL - Completado

--- sprint3sesiones/www/mysite/detail.php
+++ sprint3sesiones/www/mysite/detail.php
@@ -2,40 +2,50 @@
     $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <html>
-    <body>
-    <a href="/logout.php">Logout</a>
-        <?php
-            if (!isset($_GET['juego_id'])) {
-                die('No se ha especificado un juego');
-                }
-                $juego_id = $_GET['juego_id'];
-                $query = 'SELECT * FROM tJuegos WHERE id='.$juego_id;
-                $result = mysqli_query($db, $query) or die('Query error');
-                $only_row = mysqli_fetch_array($result);
-                echo '<h1>'.$only_row[1].'</h1>';
-                echo '<img src="'.$only_row[2].'">';
-                echo '<p>'.$only_row[3].'</p>';
-                echo '<p>'.$only_row[4].'</p>';
-                echo '<br>';
+    <head>
+        <style>
 
-            ?>
-    <h3>Comentarios:</h3>
-    <ul>
-        <?php
-            $query2 = 'SELECT * FROM tComentarios c join tUsuarios u  on u.id=c.usuario_id WHERE c.juego_id='.$juego_id;
-            $result2 = mysqli_query($db, $query2) or die('Query Error');
-            while ($row = mysqli_fetch_array($result2)) {
-                     echo '<li>'.$row['nombre'].' ('.$row['fecha_comentario'].') : ' .$row['comentario'].'</li>';
+            .conexionEStablecida{
+                font-size: 10px;
+                text-align: right;
             }
-            mysqli_close($db);
-        ?>
+        </style>
+    </head>
+        <body>
+        <h1 class="conexionEStablecida">Conexion establecida - <a href="/logout.php">Logout</a></h1>
+            <?php
+                if (!isset($_GET['juego_id'])) {
+                    die('No se ha especificado un juego');
+                    }
+                    $juego_id = $_GET['juego_id'];
+                    $query = 'SELECT * FROM tJuegos WHERE id='.$juego_id;
+                    $result = mysqli_query($db, $query) or die('Query error');
+                    $only_row = mysqli_fetch_array($result);
+                    echo '<h1>'.$only_row[1].'</h1>';
+                    echo '<img src="'.$only_row[2].'">';
+                    echo '<br>';
+                    echo '<a href="'.$only_row[3].'">Trailer</a>';
+                    echo '<p>Dificultad: '.$only_row[4].'</p>';
+                    echo '<br>';
 
-        </ul>
-        <p>Deja un nuevo comentario:</p>
-<form action="/comment.php" method="post">
-    <textarea rows="4" cols="50" name="new_comment"></textarea><br>
-    <input type="hidden" name="juego_id" value="<?php echo $juego_id; ?>">
-    <input type="submit" value="Comentar">
-</form>
-    </body>
+                ?>
+        <h3>Comentarios:</h3>
+        <ul>
+            <?php
+                $query2 = 'SELECT * FROM tComentarios c join tUsuarios u  on u.id=c.usuario_id WHERE c.juego_id='.$juego_id;
+                $result2 = mysqli_query($db, $query2) or die('Query Error');
+                while ($row = mysqli_fetch_array($result2)) {
+                        echo '<li>'.$row['nombre'].' ('.$row['fecha_comentario'].') : ' .$row['comentario'].'</li>';
+                }
+                mysqli_close($db);
+            ?>
+
+            </ul>
+            <p>Deja un nuevo comentario:</p>
+    <form action="/comment.php" method="post">
+        <textarea rows="4" cols="50" name="new_comment"></textarea><br>
+        <input type="hidden" name="juego_id" value="<?php echo $juego_id; ?>">
+        <input type="submit" value="Comentar">
+    </form>
+        </body>
 </html>
--- sprint3sesiones/www/mysite/main.php
+++ sprint3sesiones/www/mysite/main.php
@@ -1,33 +1,74 @@
 <?php
-	$db = mysqli_connect('localhost','root','1234','mysitedb') or die ('Fail');
+    $db = mysqli_connect('localhost','root','1234','mysitedb') or die ('Fail');
 ?>
-<html>
-	<body>
-		<h1>Conexion establecida</h1>
-		<a href="/logout.php">Logout</a>
-		<?php
-			// Lanzar una query
-			$query = 'SELECT * FROM tJuegos';
-			$result = mysqli_query($db, $query) or die ('Query error');
-			//Recorrer el resultado
-			while ($row = mysqli_fetch_array($result)) {
-				echo '<br>';
-				echo '<br>';
-				echo $row[1];
-				echo '<br>';
-				echo '<img src="'.$row[2].'">';
-				echo '<br>';
-				echo '<br>';
-				echo $row[3];
-				echo '<br>';
-				echo $row[4];
-				echo '<br>';
-				echo '<br>';
-				//Que cada elemento con ID <id> tenga un enlace href que redirija a la página:/detail.php?id=<id>
-				echo '<a href="detail.php?juego_id='.$row[0].'">Ver detalles</a>';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+        <style>
 
+			*{
+				box-sizing: border-box;
+				font-family: 'Poppins', sans-serif;
 			}
-		mysqli_close($db);
-		?>
-	</body>
+
+			.tituloPagina{
+				margin-top: 2%;
+				text-align: center;
+			}
+
+			.conexionEStablecida{
+				font-size: 15px;
+			}
+
+
+            .img{
+                transition: 0.3s ease-in-out;
+                width: 230px;
+				margin-top:7%;
+            }
+
+            .img:hover{
+                transform: scale(1.25);
+            }
+
+			div{
+				display: inline-block;
+				margin: 15px;
+				margin-left: 6%;
+				margin-top: 5%;
+			}
+
+			.tituloJuego{
+				font-weight: bold;
+				text-align: center;
+			}
+			
+        </style>
+    </head>
+        <body>
+            <h2 class="conexionEStablecida">Conexion establecida - <a class="enlace" href="/logout.php">Logout</a></h2>
+            <h2 class="tituloPagina">JUEGOS</h2>
+            <?php
+                // Lanzar una query
+                $query = 'SELECT * FROM tJuegos';
+                $result = mysqli_query($db, $query) or die ('Query error');
+                //Recorrer el resultado
+                while ($row = mysqli_fetch_array($result)) {
+                    //Mostrar los datos horizontales de la tabla
+                    echo '<div>';
+
+                    echo '<p class="tituloJuego">'.$row[1].'</p>';
+                    echo '<a href="detail.php?juego_id='.$row[0].'"><img class="img" src="'.$row[2].'"></a>';
+					echo '</div>';
+					
+                    
+                }
+            mysqli_close($db);
+            ?>
+        </body>
 </html>
