232d460842b133b76dd99b608a8cfc34007ac592
César Carvalhal <ccarvalhalt22@fpcoruna.afundacion.org>
2022-11-07 13:28:48 +0100

    Segundo endpoint: GET de juego por <id> - TAREA REALIZADA

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -19,3 +19,22 @@ def devolver_juegos(request):
 
 		respuesta_final.append(diccionario)
 	return JsonResponse(respuesta_final, safe=False)
+
+def devolver_juego_por_id(request, id_solicitado):
+	juego = Tjuegos.objects.get(id = id_solicitado)
+	comentarios = juego.tcomentarios_set.all()
+	lista_comentarios = []
+	for fila_comentario_sql in comentarios:
+			diccionario = {}
+			diccionario['id'] = fila_comentario_sql.id
+			diccionario['comentario'] = fila_comentario_sql.comentario
+			lista_comentarios.append(diccionario) 
+	resultado = { 
+			'id' : juego.id,
+			'titulo' : juego.nombre,
+			'dificultad' : juego.dificultad,
+			'imagen' : juego.url_imagen,
+			'trailer' : juego. url_video,
+			'comentarios' : lista_comentarios
+		}
+	return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False})
